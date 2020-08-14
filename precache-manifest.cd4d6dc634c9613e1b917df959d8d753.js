94d22f36cbb28302ee3beb2e98b041a76fb56c77
Mustafa Asif <mustafaasif1@hotmail.com>
2020-08-14 11:31:15 +0500

    Updates

---
+++ precache-manifest.cd4d6dc634c9613e1b917df959d8d753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e4a5979746792f8771fcb65a2217ef",
+    "url": "/AsliDoctor/index.html"
+  },
+  {
+    "revision": "f5934e7e8cc61cd31ea6",
+    "url": "/AsliDoctor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "47dd6248e405f0cd35d2",
+    "url": "/AsliDoctor/static/js/2.42ab5fc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AsliDoctor/static/js/2.42ab5fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5934e7e8cc61cd31ea6",
+    "url": "/AsliDoctor/static/js/main.0619d06d.chunk.js"
+  },
+  {
+    "revision": "8ec9033c3d3c9ceb78b6",
+    "url": "/AsliDoctor/static/js/runtime-main.3118dfaf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AsliDoctor/static/media/logo.5d5d9eef.svg"
+  }
+]);
